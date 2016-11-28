931f0b62167e1db246cf6ddaadeab96964375b56
mothinos <trotin.thomas@gmail.com>
2016-11-28 17:28:43 +0100

    lundi soir

---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<header>
+	<div id="header">
+		<img src="images/lokahiboards-logo-fond-blanc-longueur.jpg" alt="logo" class="logo" title="logo">
+			<nav id="menus">
+				<div id="menu">
+					<ul>
+						<li><a href="PHILOSOPHY.html">PHILOSOPHY</a></li>
+						<li><a href="DESIGN.html">DESIGN</a></li>
+						<li><a href="DESIGN.html">SHOP</a></li>
+						<li><a href="RIDERS.html">RIDERS</a></li>
+						<li><a href="RETAILERS.html">RETAILERS</a></li>
+					</ul>
+				</div>
+				<div id="icones">
+					<ul>
+						<li><a href="#"><img src="icones/ico-search.png" alt="search" class="icones"></a></li>
+						
+						<li><a href="#"><img src="icones/ico-account.png" alt="account" class="icones"></a></li>
+						
+						<li><a href="#"></a><img src="icones/ico-cart-orange-petit.png" alt="cart-orange" class="icones"></a></li>
+						
+						<li><a href="#"><img src="icones/ico-heart-bleu-contour.png"></a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+</header>
