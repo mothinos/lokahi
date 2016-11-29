6f2457d2ad078f0cc5ce14cd46028fe86603170c
Tardis\Mothinos <trotin.thomas@gmail.com>
2016-11-29 23:41:32 +0100

    mardi soir (recherche de smooth scroll en css...)

--- philosophy.php
+++ philosophy.php
@@ -3,21 +3,33 @@
 		<head>
 			<title>LOKAHI - WATER EXPLORER</title>
 			<link rel="stylesheet" type="text/css" href="style.css">
-			<link rel="stylesheet" type="text/css" href="style2.css">
-			<link rel="stylesheet" type="text/css" href="slider.css">
-
 			<link rel="icon" type="image/png" href="icones/ico-range-lokahi-mini.png">
-			<!--<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet"> -->
 			<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 		</head>
 <?php require("includes/header.php"); ?>
 		<body>
 		<div id="philosophy_bandeau"><h1>PHILOSOPHY</h1></div>
-			<h1><span>PH</span>ILOSOPHY</h1>
+			<div id="corp_philo">
+			<h1><span id="ph">PH</span>ILOSOPHY</h1>
 			<p id="passion">AT LOKAHI, WE WANT TO</br> INTRODUCE YOU TO OUR</br> PASSION</p>
-			<img src="http://lokahiboards.com/fr/wp-content/uploads/sites/10/2015/08/img-philosophie1.jpg">
-			<img src="http://lokahiboards.com/fr/wp-content/uploads/sites/10/2015/08/img-philosophie2.jpg">
-			<p>At Lokahi, we want to introduce you to our passion and share our experiences in all the stand up paddle styles that we offer: Discover, Touring, Wave, Race, and Harmony. Our philosophy and objective is to give as many people as possible the opportunity to practice this sport.</br>We have developed our products in direct collaboration with our riders. We have made our products affordable, multifunctional, and high performing so that each stand up paddler can find a board that fits his or her expectations and style.</br>Whether it be in the research of new materials or in new shapes and designs, with more than 20 years of experience in the surfing world, we have never stopped exploring new possibilities.</p>
+			
+			<img src="http://lokahiboards.com/fr/wp-content/uploads/sites/10/2015/08/img-philosophie1.jpg" alt="img-philosophie1" title="img-philosophie1">
+			<img src="http://lokahiboards.com/fr/wp-content/uploads/sites/10/2015/08/img-philosophie2.jpg" alt="img-philosophie2" title="img-philosophie2"></div>
+			<div id="paragraphe_philo"><p>At Lokahi, we want to introduce you to our passion and share our experiences in all the stand up paddle styles that we offer: Discover, Touring, Wave, Race, and Harmony. Our philosophy and objective is to give as many people as possible the opportunity to practice this sport.</br>We have developed our products in direct collaboration with our riders. We have made our products affordable, multifunctional, and high performing so that each stand up paddler can find a board that fits his or her expectations and style.</br>Whether it be in the research of new materials or in new shapes and designs, with more than 20 years of experience in the surfing world, we have never stopped exploring new possibilities.</p>
+			</div>
+			</div>
+		
+ <!--       
+
+<div class="video-content" style="background-image: url(http://lokahiboards.com/fr/wp-content/uploads/sites/10/2015/08/Sans-titre-2.jpg)">
+    <iframe src="https://www.youtube.com/embed/s_5NSMM3sD0" allowfullscreen="" height="315" frameborder="0" width="560"></iframe>
+    <div id="P1" class="video-content-video player mb_YTPlayer" data-property="{videoURL:'s_5NSMM3sD0',containment:'self',startAt:0,mute:false,autoPlay:false,loop:false,opacity:1, showYTLogo:false, stopMovieOnBlur: true, ratio:'auto', quality:'small'}" style="background: rgba(0, 0, 0, 0.5) url(&quot;https://i.ytimg.com/vi/s_5NSMM3sD0/hqdefault.jpg&quot;) repeat scroll center center / cover ;"><span id="controlBar_P1" class="mb_YTPBar inlinePlayer compact" style="white-space: nowrap; position: absolute; z-index: 1000; display: inline-block;"><div class="buttonBar"><span class="mb_YTPPlaypause ytpicon">P</span><span class="mb_YTPMuteUnmute ytpicon">M</span><div class="mb_YTPVolumeBar simpleSlider" style="display: inline-block; cursor: default;"><div class="level horizontal" style="width: 50%;"></div></div><span class="mb_YTPTime">00 : 01 / 03 : 20</span><span class="mb_OnlyYT ytpicon">O</span></div><div class="mb_YTPProgress" style="position: absolute;"><div class="mb_YTPLoaded" style="position: absolute; left: 0px; width: 12.5%;"></div><div class="mb_YTPseekbar" style="position: absolute; left: 0px; width: 1.4px;"></div></div></span><div class="mbYTP_wrapper" id="wrapper_mbYTP_P1" style="position: absolute; z-index: 0; min-width: 100%; min-height: 100%; left: 0px; top: 0px; overflow: hidden; opacity: 0;"><iframe id="mbYTP_P1" class="playerBox" style="position: absolute; z-index: 0; width: 1379.68px; height: 681px; top: 0px; left: 0px; overflow: hidden; opacity: 1; margin-top: -61.5px; margin-left: -173.84px;" allowfullscreen="1" title="YouTube video player" src="https://www.youtube.com/embed/s_5NSMM3sD0?autoplay=0&amp;modestbranding=1&amp;controls=0&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1&amp;version=3&amp;playerapiid=mbYTP_P1&amp;origin=http%3A%2F%2Flokahiboards.com&amp;allowfullscreen=true&amp;wmode=transparent&amp;iv_load_policy=3&amp;html5=1&amp;widgetid=1" height="360" frameborder="0" width="640"></iframe><div style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%;" class="YTPOverlay"></div></div>
+        <div class="video-content-bg-video" style="background-image: url(http://lokahiboards.com/fr/wp-content/uploads/sites/10/2015/08/Sans-titre-2.jpg)"></div>
+        <a class="ico-play"></a>
+    <div class="loading" style="display: block;">Ready</div></div>
+</div>-->
+
+    
 
 		</body>
 		
